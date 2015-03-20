39befbbfcef753727d087c9e4c10c0068af6f114
Dominic Suciu <domsuciu@gmail.com>
2015-03-20 13:23:54 -0700

    add setup scripts

---
+++ mpich2mpi4py_setup.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+sudo apt-get install git
+
+git clone git@github.com:domsooch/mpi_stage.git
+
+sudo python get-pip.py
+
+sudo apt-get install libcr-dev mpich2 mpich2-doc
+sudo pip install mpi4py
+
+
+
+
